78006e6f027c397fcf50c39ef1b82ae62bfbf521
KhanNoorain <khannoorainahmed@gmail.com>
2019-02-23 19:58:49 +0530

    added an object element

---
+++ script1.js
@@ -0,0 +1,21 @@
+/*var todos=[
+"clean room",
+"brush teeth",
+"exercise",
+"study",
+"eat healthy"
+];
+
+for(var i=0;i<todos.length;i++){
+	todos[i]=todos[i]+"!";
+	console.log(todos[i]);
+
+}
+todos.forEach(function(to,k){
+	console.log(to,k);
+});
+*/
+function isUserValid(hey){
+	return hey;
+}
+var answer= "The account no is "+ (isUserValid(true)?"1234":"not available.");
