5ccf17a840d690b9b92c3a3b7871ae1d17bdd246
KhanNoorain <khannoorainahmed@gmail.com>
2019-02-23 19:00:54 +0530

    Adding JavaScript file \

---
+++ script.js
@@ -0,0 +1,44 @@
+var database=[
+	{
+	name: "John",
+	password: "haha",
+	},
+	{
+	name: "William",
+	password: "haha",
+	},
+	{
+	name: "Krish",
+	password: "haha",
+	},
+];
+
+var newsfeed=[
+	{
+		username: "ooda",
+		timeline:"i just wanna code",
+	},
+	{
+		username: "Kenty",
+		timeline:"i just wanna sleep",
+	}
+	];
+var usernamePrompt=prompt("Username:");
+var passwordPrompt=prompt("password:");
+
+function signIn(user, pwd)
+{var x=false;
+	for(var i=0;i<database.length;i++){
+	if(user=== database[i].name &&
+		pwd=== database[i].password)
+	{
+		console.log(newsfeed);
+		x=true;
+	}
+	}if(x==false){
+
+		alert("Sorry! Wrong name and password");
+	
+	}
+}
+signIn(usernamePrompt, passwordPrompt);
